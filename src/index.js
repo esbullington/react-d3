2d0197b20faf9964fc9bfe2930a3453992b534fa
Eric S. Bullington <eric.s.bullington@gmail.com>
2014-11-02 16:27:14 -0500

    Corrected omitted area chart from index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 var d3 = require('d3');
+var React = require('react');
 
 exports.BarChart = require('./barchart').BarChart;
 exports.LineChart = require('./linechart').LineChart;
 exports.PieChart = require('./piechart').PieChart;
+exports.AreaChart = require('./areachart').AreaChart;
