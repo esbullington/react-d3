4063daa9c01e2158de17d670265beb003f4db297
Eric S. Bullington <eric.s.bullington@gmail.com>
2014-11-02 17:16:11 -0500

    New controller on server for serving example page

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/dist/index.html');
+});
+
 console.log('Listening on port ' + port);
 app.listen(port);
