0e4d4c11af2585c75a7e184a61f3ebd386886f04
Eric S. Bullington <eric.s.bullington@gmail.com>
2014-11-15 14:18:33 -0500

    Updated piechart test

--- tests/piechart-tests.js
+++ tests/piechart-tests.js
@@ -1,24 +1,25 @@
 /** @jsx React.DOM */
 
 var expect = require('chai').expect;  
+var _ = require('lodash');
 
 describe('PieChart', function() {
   it('renders piechart', function() {
     var React = require('react/addons');
     var PieChart = require('../src/piechart.js').PieChart;
     var Pie = require('../src/piechart.js').Pie;
-    var generate = require('../utils/datagen').generateArrayOfNumbers;
+    var generatePartsOfWhole = require('../utils/datagen').generatePartsOfWhole;;
     var TestUtils = React.addons.TestUtils;
 
     // Render a piechart using array data
-    var data = generate(5);
+    var data = generatePartsOfWhole();
     var piechart = TestUtils.renderIntoDocument(
       <PieChart data={data} width={400} height={200} />
     );
 
     // Verify that it has the same number of pies as the array's length
     var pie = TestUtils.findRenderedDOMComponentWithTag(
-      piechart, 'g');
+      piechart, 'svg');
     expect(pie).to.exist;
   });
 });
