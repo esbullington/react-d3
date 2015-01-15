4998f87179c93ff2d020375c02922c8c1b3615c9
yang-wei <yangweilim21@gmail.com>
2015-01-15 14:19:44 +0900

    Include treemap component in index

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ exports.BarChart = require('./barchart').BarChart;
 exports.LineChart = require('./linechart').LineChart;
 exports.PieChart = require('./piechart').PieChart;
 exports.AreaChart = require('./areachart').AreaChart;
+exports.Treemap = require('./treemap').Treemap;
