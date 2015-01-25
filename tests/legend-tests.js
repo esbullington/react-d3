778e684b53ae8689419f4d3d446d540ff47b82a8
Eric S. Bullington <eric.s.bullington@gmail.com>
2015-01-25 16:04:55 -0500

    Add test for legend component

---
+++ tests/legend-tests.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('Legend', function() {
+  it('renders and tests legend component', function() {
+    var React = require('react/addons');
+    var Legend = require('../src/common/legend').Legend;
+    var generate = require('../utils/datagen').generateArrayOfPoints;
+    var TestUtils = React.addons.TestUtils;
+
+    // Render a linechart using array data
+    var data = {
+      series1: generate(5),
+      series2: generate(5)
+    };
+
+    var legend = TestUtils.renderIntoDocument(
+      <Legend 
+        data={data}
+        margins={{top: 10, right: 20, bottom: 30, left: 30}}
+      /> 
+    );
+
+    // Verify that legend list exists
+    var list = TestUtils.findRenderedDOMComponentWithTag(
+      legend, 'ul');
+    expect(list).to.exist;
+
+    // Verify that it has the proper number of list items
+    var listItems = TestUtils.scryRenderedDOMComponentsWithTag(
+      legend, 'li');
+    expect(listItems).to.have.length(2);
+  });
+});
