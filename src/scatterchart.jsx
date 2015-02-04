20aa07566a62a2da08ac47b02b0b48e490e6857b
yang-wei <yangweilim21@gmail.com>
2015-02-04 16:24:22 +0900

    Clean up scatterchart by using utils.js

--- src/scatterchart.jsx
+++ src/scatterchart.jsx
@@ -9,7 +9,7 @@ var YAxis = common.YAxis;
 var Voronoi = common.Voronoi;
 var EventEmitter = require('events').EventEmitter
 var pubsub = exports.pubsub = new EventEmitter;
-var _ = require('lodash');
+var utils = require('./common/utils');
 
 var Circle = React.createClass({
 
@@ -65,7 +65,7 @@ var Circle = React.createClass({
     if (this.props.id === id) {
       this.setState({ 
         circleRadius: this.state.circleRadius * ( 5 / 4 ),
-        circleColor: this.shade(this.props.fill, -0.2)
+        circleColor: this._shade(this.props.fill, -0.2)
       });
     }
   },
@@ -79,19 +79,7 @@ var Circle = React.createClass({
     }
   },
 
-  shade: function(hex, percent) {
-    var R, G, B, red, green, blue, number;
-    var min = Math.min, round = Math.round;
-    if(hex.length !== 7) { return hex; }
-    number = parseInt(hex.slice(1), 16); 
-    R = number >> 16;
-    G = number >> 8 & 0xFF;
-    B = number & 0xFF;
-    red = min( 255, round( ( 1 + percent ) * R )).toString(16);
-    green = min( 255, round( ( 1 + percent ) * G )).toString(16);
-    blue = min( 255, round( ( 1 + percent ) * B )).toString(16);
-    return '#' + red + green + blue; 
-  } 
+  _shade: utils.shade 
 
 });
 
@@ -158,19 +146,7 @@ var ScatterChart = exports.ScatterChart = React.createClass({
     };
   },
 
-  _calculateScales: function(props, chartWidth, chartHeight, xValues, yValues) {
-
-    var xScale = d3.scale.linear()
-      .domain([d3.min([d3.min(xValues), 0]), d3.max(xValues)])
-      .range([0, chartWidth]);
-
-    var yScale = d3.scale.linear()
-      .domain([d3.min([d3.min(yValues), 0]), d3.max(yValues)])
-      .range([chartHeight, 0]);
-
-    return {xScale: xScale, yScale: yScale};
-
-  },
+  _calculateScales: utils.calculateScales,
 
   render: function() {
 
@@ -188,42 +164,10 @@ var ScatterChart = exports.ScatterChart = React.createClass({
       chartHeight = chartHeight - this.props.titleOffset;
     }
 
-    var allValues = [];
-    var xValues = [];
-    var yValues = [];
-    var coincidentCoordinateCheck = {};
-
-    for (var seriesName in this.props.data) {
-      if (this.props.data.hasOwnProperty(seriesName)) {
-        this.props.data[seriesName].forEach(function(item, idx) {
-          // Check for NaN since d3's Voronoi cannot handle NaN values
-          // Go ahead and Proceed to next iteration since we don't want NaN
-          // in allValues or in xValues or yValues
-          if (isNaN(item.x) || isNaN(item.y)) {
-            return;
-          }
-          xValues.push(item.x);
-          yValues.push(item.y);
-          var xyCoords = item.x + "-" + item.y;
-          if (xyCoords in coincidentCoordinateCheck) {
-            // Proceed to next iteration if the x y pair already exists
-            // d3's Voronoi cannot handle NaN values or coincident coords
-            // But we push them into xValues and yValues above because
-            // we still may handle them there (labels, etc.)
-            return;
-          }
-          coincidentCoordinateCheck[xyCoords] = '';
-          var pointItem = {
-            coord: {
-              x: item.x,
-              y: item.y,
-            },
-            id: seriesName + '-' + idx
-          };
-          allValues.push(pointItem);
-        })
-      }
-    }
+    var allData = utils.concatData(this.props.data), 
+        allValues = allData.allValues,
+        xValues = allData.xValues,
+        yValues = allData.yValues;
 
     // Set pubsub max listeners to total number of nodes to be created
     pubsub.setMaxListeners(xValues.length + yValues.length)
