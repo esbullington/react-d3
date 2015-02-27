5261a0f43794b872ac0b9ebb7d4f3d07d83f3c92
Eric S. Bullington <eric.s.bullington@gmail.com>
2015-02-27 13:13:06 -0500

    Move to prop mixins

--- src/linechart/LineChart.jsx
+++ src/linechart/LineChart.jsx
@@ -10,97 +10,24 @@ var Voronoi = common.Voronoi;
 var utils = require('../utils');
 var immstruct = require('immstruct');
 var DataSeries = require('./DataSeries');
-
-var Axes = React.createClass({
-
-  propTypes: {
-    xAxisClassName: React.PropTypes.string.isRequired,
-    xOrient: React.PropTypes.oneOf(['top', 'bottom']),
-    xScale: React.PropTypes.func.isRequired,
-    xHideOrigin: React.PropTypes.bool,
-    yAxisClassName: React.PropTypes.string.isRequired,
-    yOrient: React.PropTypes.oneOf(['left', 'right']),
-    yScale: React.PropTypes.func.isRequired,
-    yHideOrigin: React.PropTypes.bool,
-    chartHeight: React.PropTypes.number.isRequired,
-    chartWidth: React.PropTypes.number.isRequired,
-    fill: React.PropTypes.string,
-    stroke: React.PropTypes.string,
-    tickStroke: React.PropTypes.string,
-    strokeWidth: React.PropTypes.string
-  },
-
-  getDefaultProps: function() {
-    return {
-      axesColor: '#000'
-    };
-  },
-
-  render: function() {
-
-    var props = this.props;
-
-    return (
-        <g>
-          <YAxis
-            yAxisClassName={props.yAxisClassName}
-            yScale={props.yScale}
-            yAxisTickCount={props.yAxisTickCount}
-            yHideOrigin={props.yHideOrigin}
-            margins={props.margins}
-            width={props.chartWidth}
-            height={props.chartHeight}
-            stroke={props.axesColor}
-          />
-          <XAxis
-            xAxisClassName={props.xAxisClassName}
-            xScale={props.xScale}
-            xAxisTickCount={props.xAxisTickCount}
-            strokeWidth={props.strokeWidth}
-            xHideOrigin={props.xHideOrigin}
-            margins={props.margins}
-            width={props.chartWidth}
-            height={props.chartHeight}
-            stroke={props.axesColor}
-          />
-        </g>
-    );
-  }
-
-});
+var mixins = require('../mixins');
+var CartesianChartPropsMixin = mixins.CartesianChartPropsMixin;
 
 
 module.exports = React.createClass({
 
+  mixins: [ CartesianChartPropsMixin ],
+
   propTypes: {
-    margins: React.PropTypes.object,
-    legendOffset: React.PropTypes.number,
     pointRadius: React.PropTypes.number,
-    yHideOrigin: React.PropTypes.bool,
-    xHideOrigin: React.PropTypes.bool,
-    width: React.PropTypes.number,
-    height: React.PropTypes.number,
-    axesColor: React.PropTypes.string,
-    title: React.PropTypes.string,
     colors: React.PropTypes.func,
-    legend: React.PropTypes.bool,
-    xAccessor: React.PropTypes.func,
-    yAccessor: React.PropTypes.func,
     displayDataPoints: React.PropTypes.bool
   },
 
   getDefaultProps: function() {
     return {
-      margins: {top: 10, right: 20, bottom: 40, left: 30},
-      legendOffset: 120,
+      className: 'rd3-linechart',
       pointRadius: 3,
-      width: 400,
-      height: 200,
-      axesColor: '#000',
-      title: '',
-      colors: d3.scale.category20c(),
-      xAccessor: (d) => d.x,
-      yAccessor: (d) => d.y,
       interpolate: false,
       interpolationType: null,
       displayDataPoints: true
@@ -170,7 +97,7 @@ module.exports = React.createClass({
         height={props.height}
         title={props.title}
       >
-        <g transform={trans} className='rd3-linechart'>
+        <g transform={trans} className={props.className}>
           {dataSeriesArray}
           <Voronoi
             structure={structure}
@@ -180,23 +107,29 @@ module.exports = React.createClass({
             width={chartWidth}
             height={chartHeight}
           />
-          <Axes
-            yAxisClassName='rd3-linechart-yaxis'
-            yAxisTickCount={props.yAxisTickCount}
-            yScale={scales.yScale}
-            yHideOrigin={props.yHideOrigin}
-
-            xAxisClassName='rd3-linechart-xaxis'
-            xAxisTickCount={props.xAxisTickCount}
-            xScale={scales.xScale}
-            xHideOrigin={props.xHideOrigin}
-
-            strokeWidth="1"
-            margins={props.margins}
-            chartWidth={chartWidth}
-            chartHeight={chartHeight}
-            stroke={props.axesColor}
-          />
+          <g>
+            <YAxis
+              yAxisClassName="rd3-linechart-yaxis"
+              yScale={scales.yScale}
+              yAxisTickCount={props.yAxisTickCount}
+              yHideOrigin={props.yHideOrigin}
+              margins={props.margins}
+              width={chartWidth}
+              height={chartHeight}
+              stroke={props.axesColor}
+            />
+            <XAxis
+              xAxisClassName="rd3-linechart-xaxis"
+              xScale={scales.xScale}
+              xAxisTickCount={props.xAxisTickCount}
+              strokeWidth={props.strokeWidth}
+              xHideOrigin={props.xHideOrigin}
+              margins={props.margins}
+              width={chartWidth}
+              height={chartHeight}
+              stroke={props.axesColor}
+            />
+          </g>
         </g>
       </Chart>
     );
