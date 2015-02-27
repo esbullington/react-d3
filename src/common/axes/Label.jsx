8fdb8b02a57cd50e1c57552c5c00891676238dcf
Eric S. Bullington <eric.s.bullington@gmail.com>
2015-02-27 16:43:23 -0500

    Align ticks and labels

--- src/common/axes/Label.jsx
+++ src/common/axes/Label.jsx
@@ -7,25 +7,48 @@ module.exports = React.createClass({
 
   render() {
     var props = this.props;
-    if (props.label && (props.orient === 'top' || props.orient === 'bottom')) {
-      return (
-        <text
-          strokeWidth="0.01"
-          y={props.offset} x={props.width/2}
-          textAnchor='middle' >
-          {props.label}
-        </text>
-      );
-    } else if (props.label && (props.orient === 'left' || props.orient === 'right')) {
-      return (
-        <text
-          strokeWidth="0.01"
-          y={-props.offset} x={-props.height/2}
-          textAnchor='middle'
-          transform="rotate(270)">
-          {props.label}
-        </text>
-      );
+    var strokeWidth = "0.01";
+    if (props.label) {
+      switch (props.orient) {
+        case 'top':
+          return (
+            <text
+              strokeWidth={strokeWidth}
+              y={props.offset} x={props.width/2}
+              textAnchor='middle' >
+              {props.label}
+            </text>
+          );
+        case 'bottom':
+          return (
+            <text
+              strokeWidth={strokeWidth}
+              y={props.offset} x={props.width/2}
+              textAnchor='middle' >
+              {props.label}
+            </text>
+          );
+        case 'left':
+          return (
+            <text
+              strokeWidth={strokeWidth}
+              y={-props.offset} x={-props.height/2}
+              textAnchor='middle'
+              transform="rotate(270)">
+              {props.label}
+            </text>
+          );
+        case 'right':
+          return (
+            <text
+              strokeWidth={strokeWidth}
+              y={props.offset} x={-props.height/2}
+              textAnchor='middle'
+              transform="rotate(270)">
+              {props.label}
+            </text>
+          );
+      }
     }
     return <text/>;
   }
