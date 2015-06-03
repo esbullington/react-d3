d6180ca6d55133c85ffeb94604eccd181a18bf52
Matthew Herbst <mherbst@chegg.com>
2015-06-02 23:26:10 -0700

    Made sure all Chart related components had a className. Simplified some code. REMOVED SUPPORT OF 'DATA' BEING AN OBJECT - ONLY ARRAYS ARE ALLOWED NOW. Removed tests dealing with 'data' as an object.

--- src/common/axes/Label.jsx
+++ src/common/axes/Label.jsx
@@ -7,46 +7,76 @@ module.exports = React.createClass({
 
   displayName: 'Label',
 
+  propTypes: {
+    height:              React.PropTypes.number,
+    horizontalTransform: React.PropTypes.string,
+    label:               React.PropTypes.string.isRequired,
+    width:               React.PropTypes.number,
+    strokeWidth:         React.PropTypes.number,
+    textAnchor:          React.PropTypes.string,
+    verticalTransform:   React.PropTypes.string
+  },
+
+  getDefaultProps() {
+    return {
+      horizontalTransform: 'rotate(270)',
+      strokeWidth:         0.01,
+      textAnchor:          'middle',
+      verticalTransform:   'rotate(0)'
+    };
+  },
+
   render() {
+
     var props = this.props;
-    var strokeWidth = '0.01';
+
     if (props.label) {
       switch (props.orient) {
         case 'top':
           return (
             <text
-              strokeWidth={strokeWidth}
-              y={props.offset} x={props.width/2}
-              textAnchor='middle' >
+              strokeWidth={props.strokeWidth.toString()}
+              textAnchor={props.textAnchor}
+              transform={props.verticalTransform}
+              x={props.width / 2}
+              y={props.offset}
+            >
               {props.label}
             </text>
           );
         case 'bottom':
           return (
             <text
-              strokeWidth={strokeWidth}
-              y={props.offset} x={props.width/2}
-              textAnchor='middle' >
+              strokeWidth={props.strokeWidth.toString()}
+              textAnchor={props.textAnchor}
+              transform={props.verticalTransform}
+              x={props.width / 2}
+              y={props.offset}
+            >
               {props.label}
             </text>
           );
         case 'left':
           return (
             <text
-              strokeWidth={strokeWidth}
-              y={-props.offset} x={-props.height/2}
-              textAnchor='middle'
-              transform='rotate(270)'>
+              strokeWidth={props.strokeWidth.toString()}
+              textAnchor={props.textAnchor}
+              transform={props.horizontalTransform}
+              y={-props.offset}
+              x={-props.height / 2}
+            >
               {props.label}
             </text>
           );
         case 'right':
           return (
             <text
-              strokeWidth={strokeWidth}
-              y={props.offset} x={-props.height/2}
-              textAnchor='middle'
-              transform='rotate(270)'>
+              strokeWidth={props.strokeWidth.toString()}
+              textAnchor={props.textAnchor}
+              transform={props.horizontalTransform}
+              y={props.offset}
+              x={-props.height / 2}
+            >
               {props.label}
             </text>
           );
@@ -56,5 +86,3 @@ module.exports = React.createClass({
   }
 
 });
-
-
