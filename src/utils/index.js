b35855f6fcc835adc87c15658b15552e2ba63746
Leon Chen <lchen3@gmail.com>
2015-07-11 14:04:45 -0400

    fixes toString bug with single-digit RGB values

--- src/utils/index.js
+++ src/utils/index.js
@@ -142,8 +142,11 @@ exports.shade = (hex, percent) => {
   G = number >> 8 & 0xFF;
   B = number & 0xFF;
   red = min( 255, round( ( 1 + percent ) * R )).toString(16);
+  if (red.length === 1) red = '0' + red;
   green = min( 255, round( ( 1 + percent ) * G )).toString(16);
+  if (green.length === 1) green = '0' + green;
   blue = min( 255, round( ( 1 + percent ) * B )).toString(16);
+  if (blue.length === 1) blue = '0' + blue;
   return `#${ red }${ green }${ blue }`;
 
 };
