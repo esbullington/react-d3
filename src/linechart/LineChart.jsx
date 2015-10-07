83f89cdb59e5d0df3e9359c3b41dce45a91764ed
Nicolai Scheele <scheele@congregatio.org>
2015-10-07 14:34:21 +0200

    Recreated commits f4d4b25 and 0eb464b (lost due to merge/revert merge)

--- src/linechart/LineChart.jsx
+++ src/linechart/LineChart.jsx
@@ -17,6 +17,7 @@ module.exports = React.createClass({
     circleRadius:   React.PropTypes.number,
     hoverAnimation: React.PropTypes.bool,
     margins:        React.PropTypes.object,
+    overrideSets:   React.PropTypes.object
  },
 
   getDefaultProps() {
@@ -27,6 +28,7 @@ module.exports = React.createClass({
       margins:        {top: 10, right: 20, bottom: 50, left: 45},
       xAxisClassName: 'rd3-linechart-xaxis',
       yAxisClassName: 'rd3-linechart-yaxis',
+      overrideSets:   {}
     };
   },
 
@@ -56,9 +58,8 @@ module.exports = React.createClass({
     var allValues = flattenedData.allValues,
         xValues = flattenedData.xValues,
         yValues = flattenedData.yValues;
-    var scales = this._calculateScales(innerWidth, innerHeight, xValues, yValues);
+    var scales = this._calculateScales(innerWidth, innerHeight, xValues, yValues, props.xAxisRange, props.yAxisRange);
     var trans = "translate(" + (props.yAxisOffset < 0 ? props.margins.left + Math.abs(props.yAxisOffset) : props.margins.left) + "," + props.margins.top + ")";
-
     return (
       <Chart
         viewBox={this.getViewBox()}
@@ -128,6 +129,8 @@ module.exports = React.createClass({
             colorAccessor={props.colorAccessor}
             width={innerWidth}
             height={innerHeight}
+            xAxisRange={props.xAxisRange}
+            overrideSets={props.overrideSets}
             />
         </g>
       </Chart>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 var d3 = require('d3');
 
 
-exports.calculateScales = (chartWidth, chartHeight, xValues, yValues) => {
+exports.calculateScales = (chartWidth, chartHeight, xValues, yValues, xAxisRange, yAxisRange) => {
 
   var xScale, yScale;
 
@@ -12,7 +12,13 @@ exports.calculateScales = (chartWidth, chartHeight, xValues, yValues) => {
     xScale = d3.scale.linear()
       .range([0, chartWidth]);
   }
-  xScale.domain(d3.extent(xValues));
+
+  var xDomain = d3.extent(xValues);
+  if (xAxisRange) {
+    xDomain[0] = d3.min([d3.max([xDomain[0],xAxisRange.maxExtentLeft]),xAxisRange.minExtentLeft]);
+    xDomain[1] = d3.min([d3.max([xDomain[1],xAxisRange.minExtentRight]),xAxisRange.maxExtentRight]);
+  }
+  xScale.domain(xDomain);
 
   if (yValues.length > 0 && Object.prototype.toString.call(yValues[0]) === '[object Date]') {
     yScale = d3.time.scale()
@@ -22,7 +28,12 @@ exports.calculateScales = (chartWidth, chartHeight, xValues, yValues) => {
       .range([chartHeight, 0]);
   }
 
-  yScale.domain(d3.extent(yValues));
+  var yDomain = d3.extent(yValues);
+  if (yAxisRange) {
+    yDomain[0] = d3.min([d3.max([yDomain[0],yAxisRange.maxExtentBottom]),yAxisRange.minExtentBottom]);
+    yDomain[1] = d3.min([d3.max([yDomain[1],yAxisRange.minExtentTop]),yAxisRange.maxExtentTop]);
+  }
+  yScale.domain(yDomain);
 
   return {
     xScale: xScale,
@@ -113,7 +124,7 @@ exports.flattenData = (data, xAccessor, yAccessor) => {
       var pointItem = {
         coord: {
           x: x,
-          y: yNode,
+          y: yNode
         },
         d: item,
         id: series.name + j,
@@ -149,4 +160,22 @@ exports.shade = (hex, percent) => {
   if (blue.length === 1) blue = '0' + blue;
   return `#${ red }${ green }${ blue }`;
 
+};
+
+exports.isDate = (d, accessor) => {
+  return Object.prototype.toString.call(accessor(d)) === '[object Date]';
+};
+
+exports.prepareValues = (props,values,accessor) => {
+  var xform = props.xAccessor;
+  var yform = props.yAccessor;
+  if (!accessor) accessor = (d) => d;
+  if (exports.isDate(props.data[0].values[0], xform)) xform = function(d) { return props.xAccessor(d).getTime() };
+  if (exports.isDate(props.data[0].values[0], yform)) yform = function(d) { return props.yAccessor(d).getTime() };
+  return values.map( (v) => ({ x: xform(accessor(v)), y: yform(accessor(v)), original: v }) );
+};
+
+exports.linkValues = (values) => {
+  values.forEach( (v,idx,array) => { v.nextVal = array[idx+1]; v.prevVal = array[idx-1] } );
+  return values;
 };
