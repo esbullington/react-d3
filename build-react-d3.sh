9953a1fa437fdf76efa07772747d4ad38f514153
Christian Fässler <christian.faessler@cistec.com>
2015-12-08 11:02:06 +0100

    Adapted paths (paths changed when file was moved)

--- build-react-d3.sh
+++ build-react-d3.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # build react-d3 and upload the npm package to mobiledev-ubuntu
-cd react-d3     # git folder of react-d3
 gulp release 
 cd ..
 npm pack react-d3/build/cjs/
 scp react-d3-0.4.1.tgz $USER@mobiledev-ubuntu.cistec.com:/var/www/html/tar/
+rm -f react-d3-0.4.1.tgz
+cd -
