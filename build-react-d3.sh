f1838657363054f65f083bb41e83975685c4e91d
Christian Fässler <christian.faessler@cistec.com>
2016-01-06 08:49:27 +0100

    Removed developer specific stuff

--- build-react-d3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# build react-d3 and upload the npm package to mobiledev-ubuntu
-gulp release 
-cd ..
-npm pack react-d3/build/cjs/
-scp react-d3-0.4.2.tgz $USER@mobiledev-ubuntu.cistec.com:/var/www/html/tar/
-rm -f react-d3-0.4.2.tgz
-cd -
