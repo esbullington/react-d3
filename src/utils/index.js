915ef857adda3888fbc4dbe4cdade518e8556c27
napisani <napisani@yahoo.com>
2016-01-20 21:44:57 -0500

    started adding support for ordinal x and y axis

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,29 @@
 var d3 = require('d3');
 
 
-exports.calculateScales = (chartWidth, chartHeight, xValues, yValues) => {
+Array.prototype.unique = function() {
+  var o = {}, i, l = this.length, r = [];
+  for(i=0; i<l;i+=1) o[this[i]] = this[i];
+  for(i in o) r.push(o[i]);
+  return r;
+};
 
-  var xScale, yScale;
+exports.calculateScales = function(chartWidth, chartHeight, xValues, yValues)  {
+
+  var xScale, yScale, tickDist, tickRangeNum;
+  var tickRange = [];
 
   if (xValues.length > 0 && Object.prototype.toString.call(xValues[0]) === '[object Date]') {
     xScale = d3.time.scale()
       .range([0, chartWidth]);
+  } else if (xValues.length > 0 && Object.prototype.toString.call(xValues[0]) === '[object String]'){
+    tickDist = (chartWidth) / xValues.unique().length;
+    for (tickRangeNum = 0; tickRangeNum < chartWidth; tickRangeNum += tickDist) {
+      tickRange.push(tickRangeNum);
+    }
+    xScale = d3.scale.ordinal().domain(xValues).range(tickRange);
   } else {
+
     xScale = d3.scale.linear()
       .range([0, chartWidth]);
   }
@@ -17,7 +32,14 @@ exports.calculateScales = (chartWidth, chartHeight, xValues, yValues) => {
   if (yValues.length > 0 && Object.prototype.toString.call(yValues[0]) === '[object Date]') {
     yScale = d3.time.scale()
       .range([chartHeight, 0]);
-  } else {
+  } else if (yValues.length > 0 && Object.prototype.toString.call(yValues[0]) === '[object String]'){
+    tickDist = (chartHeight) / yValues.unique().length;
+    for (tickRangeNum = 0 ; tickRangeNum < chartHeight; tickRangeNum += tickDist) {
+      tickRange.push(tickRangeNum);
+    }
+    console.log(JSON.stringify(tickRange));
+    yScale = d3.scale.ordinal().domain(yValues).range(tickRange);
+  }else {
     yScale = d3.scale.linear()
       .range([chartHeight, 0]);
   }
@@ -52,24 +74,17 @@ exports.debounce = function(func, wait, immediate) {
   };
 };
 
-exports.flattenData = (data, xAccessor, yAccessor) => {
+exports.flattenData = function(data, xAccessor, yAccessor)  {
 
   var allValues = [];
   var xValues = [];
   var yValues = [];
   var coincidentCoordinateCheck = {};
 
-  data.forEach( (series, i) => {
-    series.values.forEach( (item, j) => {
+  data.forEach( function(series, i)  {
+    series.values.forEach( function(item, j)  {
 
       var x = xAccessor(item);
-
-      // Check for NaN since d3's Voronoi cannot handle NaN values
-      // Go ahead and Proceed to next iteration since we don't want NaN
-      // in allValues or in xValues or yValues
-      if (isNaN(x)) {
-        return;
-      }
       xValues.push(x);
 
       var y = yAccessor(item);
@@ -78,29 +93,17 @@ exports.flattenData = (data, xAccessor, yAccessor) => {
       var yNode;
       if (typeof y === 'object' && Object.keys(y).length > 0) {
         Object.keys(y).forEach(function (key) {
-          // Check for NaN since d3's Voronoi cannot handle NaN values
-          // Go ahead and Proceed to next iteration since we don't want NaN
-          // in allValues or in xValues or yValues
-          if (isNaN(y[key])) {
-            return;
-          }
           yValues.push(y[key]);
           // if multiple y points are to be plotted for a single x
           // as in the case of candlestick, default to y value of 0
           yNode = 0;
         });
       } else {
-        // Check for NaN since d3's Voronoi cannot handle NaN values
-        // Go ahead and Proceed to next iteration since we don't want NaN
-        // in allValues or in xValues or yValues
-        if (isNaN(y)) {
-          return;
-        }
         yValues.push(y);
         yNode = y;
       }
 
-      var xyCoords = `${x}-${yNode}`;
+      var xyCoords = (x + "-" + yNode);
       if (coincidentCoordinateCheck.hasOwnProperty(xyCoords)) {
         // Proceed to next iteration if the x y pair already exists
         // d3's Voronoi cannot handle NaN values or coincident coords
@@ -132,7 +135,7 @@ exports.flattenData = (data, xAccessor, yAccessor) => {
 };
 
 
-exports.shade = (hex, percent) => {
+exports.shade = function(hex, percent)  {
 
   var R, G, B, red, green, blue, number;
   var min = Math.min, round = Math.round;
@@ -147,6 +150,6 @@ exports.shade = (hex, percent) => {
   if (green.length === 1) green = '0' + green;
   blue = min( 255, round( ( 1 + percent ) * B )).toString(16);
   if (blue.length === 1) blue = '0' + blue;
-  return `#${ red }${ green }${ blue }`;
+  return ("#" +  red +  green +  blue);
 
 };
